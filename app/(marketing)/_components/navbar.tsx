8dac45b75fc6deb77c47e5a4ecb137918543730a
Ihar Anikeyenka <ianikeyenka@productiveedge.com>
2023-11-11 21:22:59 +0100

    Auth & Database

--- app/(marketing)/_components/navbar.tsx
+++ app/(marketing)/_components/navbar.tsx
@@ -4,8 +4,15 @@ import { useScrollTop } from '@/hooks/use-scroll-top'
 import { cn } from '@/lib/utils'
 import { ModeToggle } from '@/components/mode-toggle'
 import { Logo } from '@/app/(marketing)/_components/logo'
+import { useConvexAuth } from 'convex/react'
+import { SignInButton, UserButton } from '@clerk/clerk-react'
+import { Button } from '@/components/ui/button'
+import { Spinner } from '@/components/spinner'
+import Link from 'next/link'
 
 export const Navbar = () => {
+  const { isAuthenticated, isLoading } = useConvexAuth()
+  console.log(isAuthenticated)
   const scrolled = useScrollTop()
   return (
     <div
@@ -16,6 +23,27 @@ export const Navbar = () => {
     >
       <Logo />
       <div className="md:ml-auto md:justify-end justify-between w-full flex items-center gap-x-2">
+        {isLoading && <Spinner />}
+        {!isAuthenticated && !isLoading && (
+          <>
+            <SignInButton mode="modal">
+              <Button variant="ghost" size="sm">
+                Log in
+              </Button>
+            </SignInButton>
+            <SignInButton mode="modal">
+              <Button size="sm">Get MindNote free</Button>
+            </SignInButton>
+          </>
+        )}
+        {isAuthenticated && !isLoading && (
+          <>
+            <Button variant="ghost" size="sm" asChild>
+              <Link href="/documents">Enter MindNote</Link>
+            </Button>
+            <UserButton afterSignOutUrl="/" />
+          </>
+        )}
         <ModeToggle />
       </div>
     </div>
