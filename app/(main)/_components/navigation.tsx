72bbe8a99870ef5d621145552a2a69f8e20daddc
Ihar Anikeyenka <ianikeyenka@productiveedge.com>
2023-11-12 19:01:56 +0100

    Schema & API

--- app/(main)/_components/navigation.tsx
+++ app/(main)/_components/navigation.tsx
@@ -6,10 +6,13 @@ import { useMediaQuery } from 'usehooks-ts'
 import { usePathname } from 'next/navigation'
 import { cn } from '@/lib/utils'
 import { UserItem } from '@/app/(main)/_components/user-item'
+import { useQuery } from 'convex/react'
+import { api } from '@/convex/_generated/api'
 
 export const Navigation = () => {
   const pathname = usePathname()
   const isMobile = useMediaQuery('(max-width: 768px)')
+  const documents = useQuery(api.documents.get)
 
   const isResizingRef = useRef(false)
   const sidebarRef = useRef<ElementRef<'aside'>>(null)
@@ -101,9 +104,7 @@ export const Navigation = () => {
         <div>
           <UserItem />
         </div>
-        <div className="mt-4">
-          <p>Documents</p>
-        </div>
+        <div className="mt-4">{documents?.map((document) => <p key={document._id}>{document.title}</p>)}</div>
         <div
           onMouseDown={handleMouseDown}
           onClick={resetWidth}
--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import './globals.css'
 import React from 'react'
 import { ThemeProvider } from '@/components/providers/theme-provider'
 import { ConvexClientProvider } from '@/components/providers/convex-provider'
+import { Toaster } from 'sonner'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -38,6 +39,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
             disableTransitionOnChange
             storageKey="mindnote-theme-2"
           >
+            <Toaster position="bottom-center" />
             {children}
           </ThemeProvider>
         </ConvexClientProvider>
