6ad4e4787064046ea042760367089aaa29ddd9fa
Ihar Anikeyenka <ianikeyenka@productiveedge.com>
2023-11-13 01:03:36 +0100

    Document List (first part)

--- app/(main)/_components/navigation.tsx
+++ app/(main)/_components/navigation.tsx
@@ -6,15 +6,15 @@ import { useMediaQuery } from 'usehooks-ts'
 import { usePathname } from 'next/navigation'
 import { cn } from '@/lib/utils'
 import { UserItem } from '@/app/(main)/_components/user-item'
-import { useMutation, useQuery } from 'convex/react'
+import { useMutation } from 'convex/react'
 import { api } from '@/convex/_generated/api'
 import { Item } from '@/app/(main)/_components/item'
 import { toast } from 'sonner'
+import { DocumentList } from './documents-list'
 
 export const Navigation = () => {
   const pathname = usePathname()
   const isMobile = useMediaQuery('(max-width: 768px)')
-  const documents = useQuery(api.documents.get)
   const create = useMutation(api.documents.create)
 
   const isResizingRef = useRef(false)
@@ -120,7 +120,9 @@ export const Navigation = () => {
           <Item onClick={() => {}} label="Settings" icon={Settings} />
           <Item onClick={handleCreate} label="New page" icon={PlusCircle} />
         </div>
-        <div className="mt-4">{documents?.map((document) => <p key={document._id}>{document.title}</p>)}</div>
+        <div className="mt-4">
+          <DocumentList />
+        </div>
         <div
           onMouseDown={handleMouseDown}
           onClick={resetWidth}
--- convex/documents.ts
+++ convex/documents.ts
@@ -1,17 +1,28 @@
 import { mutation, query } from './_generated/server'
 import { v } from 'convex/values'
 
-export const get = query({
-  handler: async (ctx) => {
+export const getSidebar = query({
+  args: {
+    parentDocument: v.optional(v.id('documents')),
+  },
+  handler: async (ctx, args) => {
     const identity = await ctx.auth.getUserIdentity()
 
     if (!identity) {
       throw new Error('Not authenticated')
     }
 
-    return await ctx.db.query('documents').collect()
+    const userId = identity.subject
+
+    return await ctx.db
+      .query('documents')
+      .withIndex('by_user_parent', (q) => q.eq('userId', userId).eq('parentDocument', args.parentDocument))
+      .filter((q) => q.eq(q.field('isArchived'), false))
+      .order('desc')
+      .collect()
   },
 })
+
 export const create = mutation({
   args: {
     title: v.string(),
