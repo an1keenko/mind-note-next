729ed65c70f2da9a3645d20ed582df70be441101
Ihar Anikeyenka <ianikeyenka@productiveedge.com>
2023-11-22 23:10:10 +0100

    fixes 2

--- components/search-command.tsx
+++ components/search-command.tsx
@@ -1,10 +1,10 @@
-"use client";
+'use client'
 
-import { useEffect, useState } from "react";
-import { File } from "lucide-react";
-import { useQuery } from "convex/react";
-import { useRouter } from "next/navigation";
-import { useUser } from "@clerk/clerk-react";
+import { useEffect, useState } from 'react'
+import { File } from 'lucide-react'
+import { useQuery } from 'convex/react'
+import { useRouter } from 'next/navigation'
+import { useUser } from '@clerk/clerk-react'
 
 import {
   CommandDialog,
@@ -12,51 +12,49 @@ import {
   CommandGroup,
   CommandInput,
   CommandItem,
-  CommandList
-} from "@/components/ui/command";
-import { useSearch } from "@/hooks/use-search";
-import { api } from "@/convex/_generated/api";
+  CommandList,
+} from '@/components/ui/command'
+import { useSearch } from '@/hooks/use-search'
+import { api } from '@/convex/_generated/api'
 
 export const SearchCommand = () => {
-  const { user } = useUser();
-  const router = useRouter();
-  const documents = useQuery(api.documents.getSearch);
-  const [isMounted, setIsMounted] = useState(false);
+  const { user } = useUser()
+  const router = useRouter()
+  const documents = useQuery(api.documents.getSearch)
+  const [isMounted, setIsMounted] = useState(false)
 
-  const toggle = useSearch((store) => store.toggle);
-  const isOpen = useSearch((store) => store.isOpen);
-  const onClose = useSearch((store) => store.onClose);
+  const toggle = useSearch((store) => store.toggle)
+  const isOpen = useSearch((store) => store.isOpen)
+  const onClose = useSearch((store) => store.onClose)
 
   useEffect(() => {
-    setIsMounted(true);
-  }, []);
+    setIsMounted(true)
+  }, [])
 
   useEffect(() => {
     const down = (e: KeyboardEvent) => {
-      if (e.key === "k" && (e.metaKey || e.ctrlKey)) {
-        e.preventDefault();
-        toggle();
+      if (e.key === 'k' && (e.metaKey || e.ctrlKey)) {
+        e.preventDefault()
+        toggle()
       }
     }
 
-    document.addEventListener("keydown", down);
-    return () => document.removeEventListener("keydown", down);
-  }, [toggle]);
+    document.addEventListener('keydown', down)
+    return () => document.removeEventListener('keydown', down)
+  }, [toggle])
 
   const onSelect = (id: string) => {
-    router.push(`/documents/${id}`);
-    onClose();
-  };
+    router.push(`/documents/${id}`)
+    onClose()
+  }
 
   if (!isMounted) {
-    return null;
+    return null
   }
 
   return (
     <CommandDialog open={isOpen} onOpenChange={onClose}>
-      <CommandInput
-        placeholder={`Search ${user?.fullName}'s Jotion...`}
-      />
+      <CommandInput placeholder={`Search ${user?.fullName}'s MindNote...`} />
       <CommandList>
         <CommandEmpty>No results found.</CommandEmpty>
         <CommandGroup heading="Documents">
@@ -67,16 +65,8 @@ export const SearchCommand = () => {
               title={document.title}
               onSelect={() => onSelect(document._id)}
             >
-              {document.icon ? (
-                <p className="mr-2 text-[18px]">
-                  {document.icon}
-                </p>
-              ) : (
-                <File className="mr-2 h-4 w-4" />
-              )}
-              <span>
-                {document.title}
-              </span>
+              {document.icon ? <p className="mr-2 text-[18px]">{document.icon}</p> : <File className="mr-2 h-4 w-4" />}
+              <span>{document.title}</span>
             </CommandItem>
           ))}
         </CommandGroup>
